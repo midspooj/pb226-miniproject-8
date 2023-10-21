ddd53737a937a900442ecb4e4e1b7cf7dc7fc2b7
GitHub Action <action@github.com>
2023-10-21 05:53:52 +0000

    Add metric log

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import csv
 import argparse
 
+
 def process_csv(input_file, output_file):
     total = 0
     count = 0
 
-    with open(input_file, 'r') as file:
+    with open(input_file, "r") as file:
         reader = csv.reader(file)
         next(reader)  # Skip header
 
@@ -19,13 +20,14 @@ def process_csv(input_file, output_file):
 
     average = total / count if count > 0 else 0
 
-    with open(output_file, 'w') as output:
-        output.write(f'Average: {average:.2f}')
+    with open(output_file, "w") as output:
+        output.write(f"Average: {average:.2f}")
+
 
 def main():
     parser = argparse.ArgumentParser(
         description="Process CSV file and calculate average."
-        )
+    )
     parser.add_argument("input_file", type=str, help="Input CSV file path")
     parser.add_argument("output_file", type=str, help="Output text file path")
 
@@ -33,6 +35,6 @@ def main():
 
     process_csv(args.input_file, args.output_file)
 
+
 if __name__ == "__main__":
     main()
-
--- test_main.py
+++ test_main.py
@@ -3,23 +3,26 @@ import pytest
 import csv
 from main import process_csv
 
+
 @pytest.fixture
 def input_file(tmpdir):
-    file_path = os.path.join(tmpdir, 'test_input.csv')
-    with open(file_path, 'w', newline='') as csvfile:
+    file_path = os.path.join(tmpdir, "test_input.csv")
+    with open(file_path, "w", newline="") as csvfile:
         csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(['header1', 'header2'])
-        csvwriter.writerow(['row1', '10'])
-        csvwriter.writerow(['row2', '20'])
+        csvwriter.writerow(["header1", "header2"])
+        csvwriter.writerow(["row1", "10"])
+        csvwriter.writerow(["row2", "20"])
     return file_path
+
 
 @pytest.fixture
 def output_file(tmpdir):
-    return os.path.join(tmpdir, 'output.txt')
+    return os.path.join(tmpdir, "output.txt")
+
 
 def test_process_csv(input_file, output_file):
     process_csv(input_file, output_file)
 
-    with open(output_file, 'r') as f:
+    with open(output_file, "r") as f:
         content = f.read()
-        assert content == 'Average: 15.00'
+        assert content == "Average: 15.00"
